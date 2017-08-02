316c3a133fbea19b72f5bc82d711ca56773e1080
Mel Nguyen <melnguyen115@gmail.com>
2017-08-02 16:05:24 -0700

    Buttons Send Requests

--- Application/src/pages/home/home.ts
+++ Application/src/pages/home/home.ts
@@ -15,6 +15,9 @@ export class HomePage {
   posts: Observable<any>;
 
   constructor(public navCtrl: NavController, public http: Http) {
+  }
+
+  forwardRequest() {
     this.posts = this.http.get('http://68.230.95.141:8082/forward');
     this.posts
     .map(res => res.json())
@@ -23,6 +26,68 @@ export class HomePage {
     });
   }
 
+  backRequest() {
+    this.posts = this.http.get('http://68.230.95.141:8082/backward');
+    this.posts
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
+
+  leftRequest() {
+    this.posts = this.http.get('http://68.230.95.141:8082/turn-left');
+    this.posts
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
+
+  rightRequest() {
+    this.posts = this.http.get('http://68.230.95.141:8082/turn-right');
+    this.posts
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
+
+  camUp() {
+    this.posts = this.http.get('http://68.230.95.141:8082/camera-up');
+    this.posts
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
+
+  camDown() {
+    this.posts = this.http.get('http://68.230.95.141:8082/camera-down');
+    this.posts
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
+
+  camLeft() {
+    this.posts = this.http.get('http://68.230.95.141:8082/camera-left');
+    this.posts
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
+
+  camRight() {
+    this.posts = this.http.get('http://68.230.95.141:8082/camera-right');
+    this.posts
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    });
+  }
 
   ngAfterViewInit() {
     var canvas = <HTMLCanvasElement>document.getElementById('videoCanvas');
